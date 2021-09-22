91a9d530739cbe7ddc1871f382305bacdc3fd835
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-09-22 16:43:01 -0300

    criando direorios com o os

--- teste_os.py
+++ teste_os.py
@@ -7,3 +7,11 @@ print(path)
 my_files = ['account.txt', 'details.csv', 'invite.docx']
 for file_name in my_files:
     print(os.path.join('/home/rresq/tmp', file_name))
+
+# Obtendo o diretorio de trabalho atual
+current_directory = os.getcwd()
+print(f"Diretorio de trabalho atual: {current_directory}")
+
+# Criando diretorios
+# os.makedirs('/home/rresq/github/os-labs/teste_1/teste_2/teste_3')
+# os.mkdir(f"{current_directory}/new_diretory")
