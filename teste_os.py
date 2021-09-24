98983caa0655e5be233f0fc6c8a3445b634bd9cd
RodResq <rodrigoresque@yahoo.com.br>
2021-09-24 01:14:50 -0300

    continuando estudo da lib os

--- teste_os.py
+++ teste_os.py
@@ -1,4 +1,7 @@
 import os
+import datetime
+import pathlib
+import zipfile
 
 path = os.path.join('usr', 'bin', 'span')
 print(path)
@@ -38,5 +41,30 @@ print(f"o metodo dirname() retorna tudo que estiver antes da ultima barra: {dir_
 
 # Caso dirname e basename sejam necessarios
 full_path = '/home/rresq/github/os-labs/teste_os.py'
-ex_split = os.path.split(full_path)
+ex_split = os.path.split(full_path) # retorna uma tupla
 print(f"Caso dirname e basename sejam necessarios: {ex_split}")
+print(f"O arquivo test_os.py esta na tupla? {base_name in ex_split}")
+
+retorno = '/home/rresq/github/os-labs'.split(os.path.sep)
+print(retorno)
+
+# Obtendo o tamanho de um arquivo com getsize()
+tamanho = os.path.getsize('/home/rresq/github/os-labs/teste_os.py')
+print(f"O tamanho do arquivo tem: {tamanho} bytes")
+
+# Obtendo a lista de arquivo dentro de um diretorio
+list_files = os.listdir('/home/rresq/tmp')
+print(f"O diretorios contidos em tmp sao: {list_files}")
+
+# Obtendo o tamanho totoal de todos os arquivos presente em um diretorio
+total_size = 0
+for filename in os.listdir('/home/rresq/tmp'):
+    arquivo = os.path.join('/home/rresq/tmp', filename)
+    if os.path.isfile(arquivo):
+        if not zipfile.is_zipfile(arquivo):
+            tamanho_arquivo = os.path.getsize(arquivo)
+            data_criacao_arquivo = os.path.getctime(os.path.join('/home/rresq/tmp', filename))
+            data_formatada = datetime.datetime.fromtimestamp(data_criacao_arquivo)
+            print(f"O tamanho tottao do arquivo {filename} e: {tamanho_arquivo} Bytes,  e sua Data de criacao e: {data_formatada}")
+            total_size = total_size + tamanho_arquivo
+print(f"O tamanho total de todos os arquivos e: {total_size}")
