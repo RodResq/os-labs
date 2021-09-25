2c0943253127adb28838a3b1bd1287e477a92d8f
RodResq <rodrigoresque@yahoo.com.br>
2021-09-24 23:36:38 -0300

    updt

---
+++ pprint/pprint_example.py
@@ -0,0 +1,8 @@
+import pprint
+
+cats = [{'name': 'Zophie', 'desc': 'chubby'}, {'name': 'Pooka', 'desc': 'fluffy'}]
+print(pprint.pformat(cats))
+
+file_obj = open('myCats.py', 'w')
+file_obj.write('cats = ' + pprint.pformat(cats) + '\n')
+file_obj.close()
