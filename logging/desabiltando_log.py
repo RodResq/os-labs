5fbc229a20d693763608665ec31980bfadfb0196
RodResq <rodrigoresque@yahoo.com.br>
2021-10-01 23:16:29 -0300

    desabilitado log

---
+++ logging/desabiltando_log.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+
+logging.critical('Critical error! Critical error!')
+logging.disable(logging.CRITICAL)
+logging.critical('Critical error! Critical error!')
+logging.error('Error, Error!')
